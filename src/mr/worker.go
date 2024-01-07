919d0bf0758ccf7968f2babcca4216c9a6e7a863
PeterYaoNYU <yy4108@nyu.edu>
2024-01-07 10:46:55 +0800

    worker mostly done

--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,10 +1,18 @@
 package mr
 
-import "fmt"
-import "log"
-import "net/rpc"
-import "hash/fnv"
+import (
+	"encoding/json"
+	"fmt"
+	"hash/fnv"
+	"io/ioutil"
+	"log"
+	"net/rpc"
+	"os"
+	"path/filepath"
+)
 
+// Used to store intermediate MR output
+const TempDir = "temp"
 
 //
 // Map functions return a slice of KeyValue.
@@ -24,6 +32,12 @@ func ihash(key string) int {
 	return int(h.Sum32() & 0x7fffffff)
 }
 
+func requestTask() (reply *RequestTaskReply, succ bool) {
+	args := RequestTaskArgs{WorkerID: os.Getpid()}
+	reply = &RequestTaskReply{}
+	succ = call("Coordinator.RequestTask", &args, reply)
+	return reply, succ
+}
 
 //
 // main/mrworker.go calls this function.
@@ -33,9 +47,139 @@ func Worker(mapf func(string, string) []KeyValue,
 
 	// Your worker implementation here.
 
+	for {
+		// 1. Request a task from the coordinator
+		reply, succ := requestTask()
+
+		if succ == false {
+			fmt.Println("Request task failed")
+			return
+		}
+
+		if reply.TaskType == ExitTask {
+			fmt.Println("No task left")
+			return
+		}
+
+		if reply.TaskType == MapTask {
+			// 2. Do the map task
+			doMap(mapf, reply.File, reply.TaskId, reply.nReduce)
+			// 3. Report the task is done to the coordinator
+			reportMapDone(reply.TaskId)
+		} else {
+			// 2. Do the reduce task
+			doReduce(reducef, reply.TaskId)
+			// 3. Report the task is done to the coordinator
+			reportReduceDone()
+		}
+
+		// 4. Report the task is done to the coordinator
+		// 5. Go back to step 1
+
+	}
+
 	// uncomment to send the Example RPC to the coordinator.
 	// CallExample()
 
+}
+
+func doReduce(reducef func(string, []string) string, reduceID int) {
+	// 1. Read all the intermediate files
+	files, err := filepath.Glob(fmt.Sprintf("%v/mr-*-%d", TempDir, reduceID))
+	if err != nil {
+		log.Fatalf("cannot read temp files")
+	}
+	// 2. Group the files by key
+
+	intermediate := make(map[string][]string)
+
+	for _, filepath := range files {
+		file, err := os.Open(filepath)
+		if err != nil {
+			log.Fatalf("cannot open temp file")
+		}
+
+		dec := json.NewDecoder(file)
+		for {
+			var kv KeyValue
+			err := dec.Decode(&kv)
+			if err != nil {
+				break
+			}
+			intermediate[kv.Key] = append(intermediate[kv.Key], kv.Value)
+		}
+
+		file.Close()
+	}
+
+	// 3. Call reducef on each group
+	outputFile, err := ioutil.TempFile(TempDir, "mr-out-")
+	if err != nil {
+		log.Fatalf("cannot create temp file")
+	}
+	defer outputFile.Close()
+
+	for key, values := range intermediate {
+		output := reducef(key, values)
+		fmt.Fprintf(outputFile, "%v %v\n", key, output)
+	}
+
+	// 4. Write the output to a file
+	finalFilename := fmt.Sprintf("mr-out-%d", reduceID)
+	os.Rename(outputFile.Name(), finalFilename)
+}
+
+func reportMapDone(TaskId int) {
+	args := ReportTaskDoneArgs{TaskType: MapTask, TaskId: TaskId}
+	reply := ReportTaskDoneReply{}
+	call("Coordinator.ReportTaskDone", &args, &reply)
+}
+
+func doMap(mapf func(string, string) []KeyValue, filename string, mapID int, nReduce int) {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("cannot open %v", filename)
+	}
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatalf("cannot read %v", filename)
+	}
+	file.Close()
+	kva := mapf(filename, string(content))
+
+	writeMapOutput(kva, mapID, nReduce)
+}
+
+func writeMapOutput(kva []KeyValue, mapID int, nReduce int) {
+	tempFiles := make([]*os.File, nReduce)
+	encoders := make([]*json.Encoder, nReduce)
+
+	for i := 0; i < nReduce; i++ {
+		tempFile, err := ioutil.TempFile(TempDir, "intermediate-")
+		if err != nil {
+			log.Fatalf("cannot create temp file")
+		}
+		tempFiles[i] = tempFile
+		encoders[i] = json.NewEncoder(tempFiles[i])
+	}
+
+	for _, kv := range kva {
+		reduceID := ihash(kv.Key) % nReduce
+		err := encoders[reduceID].Encode(&kv)
+		if err != nil {
+			log.Fatalf("cannot encode kv")
+		}
+		err = encoders[reduceID].Encode(&kv)
+		if err != nil {
+			log.Fatalf("cannot encode kv")
+		}
+	}
+
+	for i, tempFile := range tempFiles {
+		tempFile.Close()
+		filename := fmt.Sprintf("%v/mr-%d-%d", TempDir, mapID, i)
+		os.Rename(tempFile.Name(), filename)
+	}
 }
 
 //
